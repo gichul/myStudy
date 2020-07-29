3c68bdb1484f93a81e4d81cf2c396173e790ab04
gitcheol <kgc5551@naver.com>
2020-07-29 20:29:02 +0900

    AddeD

---
+++ read.py
@@ -0,0 +1,9 @@
+f=open("./Explosion008_x264.txt",'r')
+
+a=f.read()
+
+print(a)
+
+b=a.split()
+
+print(len(b))
---
+++ result/read.py
@@ -0,0 +1,9 @@
+f=open("./Explosion008_x264_1005.000000.csv",'r')
+
+a=f.read()
+
+print(a)
+
+b=a.split(',')
+
+print(len(b))
